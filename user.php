3932440071a6dabc000d66a8cfc82e4a86020094
myoshida-2024 <yoshida-m@mva.biglobe.ne.jp>
2025-01-14 21:40:10 +0900

    ユーザ登録メールアドレスに変更

--- user.php
+++ user.php
@@ -31,7 +31,7 @@ include "funcs.php";
    <fieldset>
     <legend>ユーザー登録</legend>
      <label>名前：<input type="text" name="username" required></label><br>
-     <label>Login ID：<input type="text" name="lid" required></label><br>
+     <label>Login ID (メールアドレス)：<input type="text" name="lid" required></label><br>
      <label>Login PW<input type="password" name="lpw" required></label><br>
      <label>管理FLG：
       一般<input type="radio" name="kanri_flg" value="0" checked>　
